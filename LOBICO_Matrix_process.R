cdc8846ae82988d4db3fd35222dedb53ae1f1501
Zhao Tianxiao <zhao-tx16@mails.tsinghua.edu.cn>
2019-01-24 10:19:29 +0800

    Update plot

--- LOBICO_Matrix_process.R
+++ LOBICO_Matrix_process.R
@@ -1,22 +1,9 @@
 dat <- read.table("~/Downloads/cpm_selected_features.Normal-CRC.txt", sep = '\t', header = T, row.names = 1)
 adjust_bi_data <- dat
-#bi_data <- dat
-#for(i in 1:nrow(dat)){
-  for(j in 1:ncol(dat)){
-    if(abs(dat[i,j]) >= 0.8){
-      bi_data[i,j] = 1
-    }
-    else{
-      bi_data[i,j] = 0
-    }
-  }
-}
-#write.table(bi_data, "~/Downloads/bi_cpm_selected_features.Normal-CRC.txt", sep = "\t", row.names = T, col.names = T)
-
 a = nrow(dat)
 b = ncol(dat)
 for(i in 1:a){
-  temp <- sum(dat[i,])/a + 2 * sd(dat[i,])
+  temp <- sum(dat[i,])/a + sd(dat[i,])
   for(j in 1:b){
     if(dat[i,j] >= temp){
       adjust_bi_data[i,j] <- 1
---
+++ data/long.random.forest.any/binarization.R
@@ -0,0 +1,68 @@
+normal_CRC <- read.table("~/LOBICO/data/long.random.forest.any/cpm_selected_features.Normal-CRC.txt", 
+                  sep = '\t', header = T, row.names = 1)
+normal_HCC <- read.table("~/LOBICO/data/long.random.forest.any/cpm_selected_features.Normal-HCC.txt", 
+                         sep = '\t', header = T, row.names = 1)
+normal_PAAD <- read.table("~/LOBICO/data/long.random.forest.any/cpm_selected_features.Normal-PAAD.txt", 
+                         sep = '\t', header = T, row.names = 1)
+## Normal-CRC part
+bi_normal_CRC <- normal_CRC
+a = nrow(bi_normal_CRC)
+b = ncol(bi_normal_CRC)
+for(i in 1:a){
+  temp <- sum(normal_CRC[i,])/a + sd(normal_CRC[i,])
+  for(j in 1:b){
+    if(normal_CRC[i,j] >= temp){
+      bi_normal_CRC[i,j] <- 1
+    }
+    else{
+      bi_normal_CRC[i,j] <- 0
+    }
+  }
+}
+write.table(bi_normal_CRC, "~/LOBICO/data/long.random.forest.any/Bi-Normal-CRC.txt",
+            sep = "\t", row.names = T, col.names = T)
+normal_CRC_class <- c(rep(0, times = 32), rep(1, times = 12))
+write.table(normal_CRC_class, "~/LOBICO/data/long.random.forest.any/Normal-CRC-biclass.txt",
+            sep = "\t", row.names = F, col.names = F)
+
+## Normal-HCC part
+bi_normal_HCC <- normal_HCC
+a = nrow(bi_normal_HCC)
+b = ncol(bi_normal_HCC)
+for(i in 1:a){
+  temp <- sum(normal_HCC[i,])/a + sd(normal_HCC[i,])
+  for(j in 1:b){
+    if(normal_HCC[i,j] >= temp){
+      bi_normal_HCC[i,j] <- 1
+    }
+    else{
+      bi_normal_HCC[i,j] <- 0
+    }
+  }
+}
+write.table(bi_normal_HCC, "~/LOBICO/data/long.random.forest.any/Bi-Normal-HCC.txt",
+            sep = "\t", row.names = T, col.names = T)
+normal_HCC_class <- c(rep(0, times = 32), rep(1, times = 21))
+write.table(normal_HCC_class, "~/LOBICO/data/long.random.forest.any/Normal-HCC-biclass.txt",
+            sep = "\t", row.names = F, col.names = F)
+
+## Normal-PAAD part
+bi_normal_PAAD <- normal_PAAD
+a = nrow(bi_normal_PAAD)
+b = ncol(bi_normal_PAAD)
+for(i in 1:a){
+  temp <- sum(normal_PAAD[i,])/a + sd(normal_PAAD[i,])
+  for(j in 1:b){
+    if(normal_PAAD[i,j] >= temp){
+      bi_normal_PAAD[i,j] <- 1
+    }
+    else{
+      bi_normal_PAAD[i,j] <- 0
+    }
+  }
+}
+write.table(bi_normal_PAAD, "~/LOBICO/data/long.random.forest.any/Bi-Normal-PAAD.txt",
+            sep = "\t", row.names = T, col.names = T)
+normal_PAAD_class <- c(rep(0, times = 32), rep(1, times = 14))
+write.table(normal_PAAD_class, "~/LOBICO/data/long.random.forest.any/Normal-PAAD-biclass.txt",
+            sep = "\t", row.names = F, col.names = F)
